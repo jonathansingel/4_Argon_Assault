b7b05a1a439d3df198ba9288dcd710bebbacddca
JonathanSingel <jonathansingel@gmail.com>
2018-03-05 18:10:03 -0500

    10 more lessons in

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] AudioClip splashScreenMusic;
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(splashScreenMusic);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float levelDelay = 8.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGameSequence();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void StartGameSequence()
+    {        
+        Invoke("LoadNextLevel", levelDelay);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex;
+
+        int totalScenes = SceneManager.sceneCountInBuildSettings;
+
+        if (currentSceneIndex + 1 < totalScenes)
+        {
+            nextSceneIndex = currentSceneIndex + 1;
+        }
+        else
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+
+
+}
