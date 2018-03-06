81915076c68c6cf7365c7a60d6ba8918d925f259
JonathanSingel <jonathansingel@gmail.com>
2018-03-05 21:01:11 -0500

    derrr

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 2f;
+    [Tooltip("FX prefab on Player Ship")][SerializeField] GameObject explosion;
+
+    // Use this for initialization
+    void Start ()
+    {    
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        explosion.SetActive(true);
+        SendMessage("OnPlayerDeath");
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+    
+    private void ReloadLevel() //string referenced
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 16f;
-    [Tooltip("In m")] [SerializeField] float xRange = 6.5f;
-
-    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 16f;
+    [Header("General")]
+    [Tooltip("In ms^-1")][SerializeField] float controlSpeed = 15f;
+    [Tooltip("In m")] [SerializeField] float xRange = 9f;
     [Tooltip("In m")] [SerializeField] float yRange = 5f;
 
-    [SerializeField] float positionPitchFactor = -3f;
-    [SerializeField] float controlPitchFactor = -30f;
+    [Header("Screen-position Based")]
+    [SerializeField] float positionPitchFactor = -5f;
     [SerializeField] float positionYawFactor = 6.5f;
+
+    [Header("Control-throw Based")]
     [SerializeField] float controlRollFactor = -30f;
+    [SerializeField] float controlPitchFactor = -30f;
 
     float xThrow, yThrow;
+    bool isControleEnabled = true;
 
     // Use this for initialization
     void Start () {
-		
+
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update ()
     {
-        ProcessTranslation();
-        ProcessRotation();
+        if (isControleEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+    }
+
+    void OnPlayerDeath() // called by string reference
+    {
+        print("Controls frozen");
+        isControleEnabled = false;
     }
 
     private void ProcessRotation()
@@ -49,14 +61,14 @@ public class Player : MonoBehaviour
     private void ProcessTranslation()
     {
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
 
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
 
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
