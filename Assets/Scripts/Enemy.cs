98fdcf1b5f18c033e4d519117e76142bf378ed1c
JonathanSingel <jonathansingel@gmail.com>
2018-03-06 18:12:41 -0500

    scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,16 +7,27 @@ public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject enemyExplosion;
     [SerializeField] Transform parent;
+    [SerializeField] int pointsPerHit;
+
+    ScoreBoard scoreBoard;
 
 	// Use this for initialization
 	void Start ()
     {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
         BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
     }
-    
+
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.addPoints(pointsPerHit);
+
         GameObject explosion = Instantiate(enemyExplosion, transform.position, Quaternion.identity);
         explosion.transform.parent = parent;
         Destroy(gameObject);
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    Text scoreText;
+    
+	// Use this for initialization
+	void Start () {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+	}
+	
+    public void addPoints(int points)
+    {
+        score = score + points;
+        scoreText.text = score.ToString();
+    }
+}
